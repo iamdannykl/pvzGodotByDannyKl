1ed5f848cd34a578070300957502dac2e8cdd817
iamdannykl <p15031090805@outlook.com>
2024-10-25 08:35:31 +0800

    daZuiHuaSprite

--- .godot/mono/temp/obj/Debug/Godot Projects.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Godot Projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Godot Projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc2c3492fc2c340ce1d3946a5eee982086c14d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c568cf555780fe4221e5691db343dc2ac6e80d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Godot Projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Godot Projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
